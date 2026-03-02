af8083214ffe50633f87fc3ce74272d08f682c3c
Juris <jurisbandenieks90@gmail.com>
2026-03-02 13:51:03 -0800

    feat(client): integrate AgGridProvider for enhanced grid functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,8 @@
 import { MantineProvider } from "@mantine/core";
 import { ModalsProvider } from "@mantine/modals";
+import { AllCommunityModule } from "ag-grid-community";
+import { AgGridProvider } from "ag-grid-react";
+
 import AppRoutes from "./Routes";
 import { theme } from "./theme";
 
@@ -11,12 +14,15 @@ import "@mantine/dates/styles.css";
 
 import { Notifications } from "@mantine/notifications";
 
+const modules = [AllCommunityModule];
+
 function App() {
   return (
     <MantineProvider theme={theme}>
       <ModalsProvider>
-        <AppRoutes />
-
+        <AgGridProvider modules={modules}>
+          <AppRoutes />
+        </AgGridProvider>
         <Notifications />
       </ModalsProvider>
     </MantineProvider>
